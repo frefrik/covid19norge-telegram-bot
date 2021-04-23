377c7d78f59d5b09c2b1306f74c8119e251332ed
Fredrik Haarstad <fredrik@haarstad.no>
2021-04-23 10:49:40 +0200

    Create bot error handler

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 import os
-from datetime import time
+import time
+import html
+import json
 import logging
-from telegram.ext import Updater, CommandHandler
+import traceback
+from datetime import time
+from telegram import ParseMode, Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 from modules.utils import load_config, file_write
 from modules.utils import job_initiate, job_enable, wait_seconds
 import modules.c19api as c19api
@@ -11,11 +16,35 @@ import jobs
 cfg = load_config()
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    level=logging.WARN, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
 logger = logging.getLogger(__name__)
 bot = cfg["bot"]
+
+
+def error_handler(update: object, context: CallbackContext) -> None:
+    logger.error(msg="Exception while handling an update:", exc_info=context.error)
+
+    tb_list = traceback.format_exception(
+        None, context.error, context.error.__traceback__
+    )
+    tb_string = "".join(tb_list)
+
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+
+    message = (
+        f"An exception was raised while handling an update\n"
+        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+        "</pre>\n\n"
+        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+        f"<pre>{html.escape(tb_string)}</pre>"
+    )
+
+    context.bot.send_message(
+        chat_id=bot["chatid_dev"], text=message, parse_mode=ParseMode.HTML
+    )
 
 
 def check_files_exist():
@@ -63,6 +92,7 @@ def main():
 
     jq.run_daily(jobs.stats, time(hour=22, minute=30))
 
+    dp.add_error_handler(error_handler)
     updater.start_polling()
     updater.idle()
 
